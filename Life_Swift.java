bcf63903897a2575d2f0bd25b08e7953cbb37750
FarhanaMaha786 <ifakhrul1991@gmail.com>
2025-11-21 17:21:59 +0600

    Add files via upload
    Welcome Page, User SignUp, NGO SignUp, User Details, NGO Details

--- Life_Swift.java
+++ Life_Swift.java
@@ -1,7 +1,19 @@
 package com.mycompany.life_swift;
-public class Life_Swift {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import java.sql.*;
+
+public class Life_Swift {
+Connection conn;
+  public static Connection connectDB(){
+   
+   try{
+       Class.forName("com.mysql.cj.jdbc.Driver");
+       Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/life_swift","root","");
+       System.out.println("Connection Successful");
+       return conn;
+   } catch(Exception e){
+       System.out.println(e);
+       return null;
+   }     
+  } 
 }
